b113b50f33c9cac6e72b33ddb57dbbdeba103ccb
Robert Wunsch <wunsch@adobe.com>
2017-01-05 18:11:17 +0000

    change docker repo name in run script.

--- docker-run.sh
+++ docker-run.sh
@@ -1,1 +1,1 @@
-docker run --name ldap --network=dockerfiles4aem_aem-network -d -p 10389:10389 h3nrik/apacheds
+docker run --name ldap --network=dockerfiles4aem_aem-network -d -p 10389:10389 rwunsch/apacheds-docker
