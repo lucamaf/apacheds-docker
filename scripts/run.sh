62cc768693eb8b7641917b1e6413e97a8804cf8b
Robert Wunsch <wunsch@adobe.com>
2017-01-06 03:23:22 +0100

    remove import of 20 k users from run.sh - takes to long

--- scripts/run.sh
+++ scripts/run.sh
@@ -15,10 +15,10 @@ if [ ! -d ${APACHEDS_INSTANCE_DIRECTORY} ]; then
     mkdir ${APACHEDS_INSTANCE_DIRECTORY}
     cp -rv ${APACHEDS_BOOTSTRAP}/* ${APACHEDS_INSTANCE_DIRECTORY}
     chown -v -R ${APACHEDS_USER}:${APACHEDS_GROUP} ${APACHEDS_INSTANCE_DIRECTORY}
-	/opt/apacheds-${APACHEDS_VERSION}/bin/apacheds console ${APACHEDS_INSTANCE} &
-	sleep 10
-	ldapadd -v -h localhost:10389 -c -x -D uid=admin,ou=system -w secret -f /bootstrap/startup-entry.ldif
-	/opt/apacheds-${APACHEDS_VERSION}/bin/apacheds stop ${APACHEDS_INSTANCE}
+	#/opt/apacheds-${APACHEDS_VERSION}/bin/apacheds console ${APACHEDS_INSTANCE} &
+	#sleep 10
+	#ldapadd -v -h localhost:10389 -c -x -D uid=admin,ou=system -w secret -f /bootstrap/startup-entry.ldif
+	#/opt/apacheds-${APACHEDS_VERSION}/bin/apacheds stop ${APACHEDS_INSTANCE}
 fi
 
 # Execute the server in console mode and not as a daemon.
